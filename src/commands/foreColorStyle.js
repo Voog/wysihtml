52f583f8ee2a7097df59197fc653082f28957d58
Oliver Pulges <oliver@fraktal.ee>
2013-11-05 11:52:46 +0200

    forecolor regex improvement

--- src/commands/foreColorStyle.js
+++ src/commands/foreColorStyle.js
@@ -4,7 +4,7 @@
  * Instead we set a css class
  */
 (function(wysihtml5) {
-  var REG_EXP = /(^|\s)color\s*\:\s*((#[0-9a-f]{3}([0-9a-f]{3})?)|(rgba?\(\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*\d{1,3}(\s*,\s*\d{1,3}\s*)?\)))\s*;?/i;
+  var REG_EXP = /(^|\s|;)color\s*\:\s*((#[0-9a-f]{3}([0-9a-f]{3})?)|(rgba?\(\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*\d{1,3}(\s*,\s*\d{1,3}\s*)?\)))\s*;?/i;
   
   wysihtml5.commands.foreColorStyle = {
     exec: function(composer, command, color) {
