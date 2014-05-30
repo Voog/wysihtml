2616039dab8f9b41648cfb645b615a21db7e52c2
Oliver Pulges <oliver@fraktal.ee>
2014-05-30 16:19:40 +0300

    Add commands indentList and outdentList for consistent list level handling behaviour

---
+++ src/commands/indentList.js
@@ -0,0 +1,41 @@
+wysihtml5.commands.indentList = {
+  exec: function(composer, command, value) {
+    var listEls = composer.selection.getSelectionParentsByTag('LI');
+    if (listEls) {
+      return this.tryToPushLiLevel(listEls, composer.selection);
+    }
+    return false;
+  },
+
+  state: function(composer, command) {
+      return false;
+  },
+
+  tryToPushLiLevel: function(liNodes, selection) {
+    var listTag, list, prevLi, liNode, prevLiList,
+        found = false;
+
+    selection.executeAndRestoreRangy(function() {
+
+      for (var i = liNodes.length; i--;) {
+        liNode = liNodes[i];
+        listTag = (liNode.parentNode.nodeName === 'OL') ? 'OL' : 'UL';
+        list = liNode.ownerDocument.createElement(listTag);
+        prevLi = wysihtml5.dom.getPreviousElement(liNode);
+        prevLiList = (prevLi) ? prevLi.querySelector('ul, ol') : null;
+
+        if (prevLi) {
+          if (prevLiList) {
+            prevLiList.appendChild(liNode);
+          } else {
+            list.appendChild(liNode);
+            prevLi.appendChild(list);
+          }
+          found = true;
+        }
+      }
+
+    });
+    return found;
+  }
+};
