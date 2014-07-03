c33bc6c48f997f8d7d7eafc289c32254c738ce21
Oliver Pulges <oliver@fraktal.ee>
2014-07-03 14:35:22 +0300

    Cures safari and Firefox focus problems with placeholder

--- src/dom/simulate_placeholder.js
+++ src/dom/simulate_placeholder.js
@@ -18,7 +18,13 @@
         unset = function() {
           if (view.hasPlaceholderSet()) {
             view.clear();
-            view.selection.selectNode(view.element.firstChild);
+            view.element.focus();
+            setTimeout(function() {
+              var sel = view.selection.getSelection();
+              if (!sel.focusNode || !sel.anchorNode) {
+                view.selection.selectNode(view.element.firstChild || view.element);
+              }
+            }, 0);
           }
           view.placeholderSet = false;
           dom.removeClass(view.element, CLASS_NAME);
