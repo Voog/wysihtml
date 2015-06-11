5ad1666202a8ee1d567cd96d84afd9384be518b6
Robert White <hi@rob.pw>
2015-06-11 17:59:51 +0100

    [Bug Fix] Dialog not updating
    For example, when clicking between a tags the dialog does not update.

--- src/toolbar/dialog.js
+++ src/toolbar/dialog.js
@@ -160,18 +160,17 @@
      * Show the dialog element
      */
     show: function(elementToChange) {
-      if (dom.hasClass(this.link, CLASS_NAME_OPENED)) {
-        return;
-      }
-
       var that        = this,
           firstField  = this.container.querySelector(SELECTOR_FORM_ELEMENTS);
+
       this.elementToChange = elementToChange;
       this._observe();
       this._interpolate();
+
       if (elementToChange) {
         this.interval = setInterval(function() { that._interpolate(true); }, 500);
       }
+
       dom.addClass(this.link, CLASS_NAME_OPENED);
       this.container.style.display = "";
       this.fire("show");
