3eac1edabc04852e7c90cb51a835b91da4e33844
Oliver Pulges <oliver@fraktal.ee>
2016-05-06 12:57:23 +0300

    Make defaults configurable (for modules)

--- src/editor.js
+++ src/editor.js
@@ -2,7 +2,7 @@
  * WYSIHTML5 Editor
  *
  * @param {Element} editableElement Reference to the textarea which should be turned into a rich text interface
- * @param {Object} [config] See defaultConfig object below for explanation of each individual config option
+ * @param {Object} [config] See defaults object below for explanation of each individual config option
  *
  * @events
  *    load
@@ -32,70 +32,70 @@
 (function(wysihtml) {
   var undef;
 
-  var defaultConfig = {
-    // Give the editor a name, the name will also be set as class name on the iframe and on the iframe's body
-    name:                 undef,
-    // Whether the editor should look like the textarea (by adopting styles)
-    style:                true,
-    // Id of the toolbar element, pass falsey value if you don't want any toolbar logic
-    toolbar:              undef,
-    // Whether toolbar is displayed after init by script automatically.
-    // Can be set to false if toolobar is set to display only on editable area focus
-    showToolbarAfterInit: true,
-    // With default toolbar it shows dialogs in toolbar when their related text format state becomes active (click on link in text opens link dialogue)
-    showToolbarDialogsOnSelection: true,
-    // Whether urls, entered by the user should automatically become clickable-links
-    autoLink:             true,
-    // Includes table editing events and cell selection tracking
-    handleTables:         true,
-    // Tab key inserts tab into text as default behaviour. It can be disabled to regain keyboard navigation
-    handleTabKey:         true,
-    // Object which includes parser rules to apply when html gets cleaned
-    // See parser_rules/*.js for examples
-    parserRules:          { tags: { br: {}, span: {}, div: {}, p: {}, b: {}, i: {}, u: {} }, classes: {} },
-    // Object which includes parser when the user inserts content via copy & paste. If null parserRules will be used instead
-    pasteParserRulesets: null,
-    // Parser method to use when the user inserts content
-    parser:               wysihtml.dom.parse,
-    // By default wysihtml5 will insert a <br> for line breaks, set this to false to use <p>
-    useLineBreaks:        true,
-    // Double enter (enter on blank line) exits block element in useLineBreaks mode.
-    // It enables a way of escaping out of block elements and splitting block elements
-    doubleLineBreakEscapesBlock: true,
-    // Array (or single string) of stylesheet urls to be loaded in the editor's iframe
-    stylesheets:          [],
-    // Placeholder text to use, defaults to the placeholder attribute on the textarea element
-    placeholderText:      undef,
-    // Whether the rich text editor should be rendered on touch devices (wysihtml5 >= 0.3.0 comes with basic support for iOS 5)
-    supportTouchDevices:  true,
-    // Whether senseless <span> elements (empty or without attributes) should be removed/replaced with their content
-    cleanUp:              true,
-    // Whether to use div instead of secure iframe
-    contentEditableMode: false,
-    classNames: {
-      // Class name which should be set on the contentEditable element in the created sandbox iframe, can be styled via the 'stylesheets' option
-      composer: "wysihtml5-editor",
-      // Class name to add to the body when the wysihtml5 editor is supported
-      body: "wysihtml5-supported",
-      // classname added to editable area element (iframe/div) on creation
-      sandbox: "wysihtml5-sandbox",
-      // class on editable area with placeholder
-      placeholder: "wysihtml5-placeholder",
-      // Classname of container that editor should not touch and pass through
-      uneditableContainer: "wysihtml5-uneditable-container"
+  wysihtml.Editor = wysihtml.lang.Dispatcher.extend({
+    /** @scope wysihtml.Editor.prototype */
+    defaults: {
+      // Give the editor a name, the name will also be set as class name on the iframe and on the iframe's body
+      name:                 undef,
+      // Whether the editor should look like the textarea (by adopting styles)
+      style:                true,
+      // Id of the toolbar element, pass falsey value if you don't want any toolbar logic
+      toolbar:              undef,
+      // Whether toolbar is displayed after init by script automatically.
+      // Can be set to false if toolobar is set to display only on editable area focus
+      showToolbarAfterInit: true,
+      // With default toolbar it shows dialogs in toolbar when their related text format state becomes active (click on link in text opens link dialogue)
+      showToolbarDialogsOnSelection: true,
+      // Whether urls, entered by the user should automatically become clickable-links
+      autoLink:             true,
+      // Includes table editing events and cell selection tracking
+      handleTables:         true,
+      // Tab key inserts tab into text as default behaviour. It can be disabled to regain keyboard navigation
+      handleTabKey:         true,
+      // Object which includes parser rules to apply when html gets cleaned
+      // See parser_rules/*.js for examples
+      parserRules:          { tags: { br: {}, span: {}, div: {}, p: {}, b: {}, i: {}, u: {} }, classes: {} },
+      // Object which includes parser when the user inserts content via copy & paste. If null parserRules will be used instead
+      pasteParserRulesets: null,
+      // Parser method to use when the user inserts content
+      parser:               wysihtml.dom.parse,
+      // By default wysihtml5 will insert a <br> for line breaks, set this to false to use <p>
+      useLineBreaks:        true,
+      // Double enter (enter on blank line) exits block element in useLineBreaks mode.
+      // It enables a way of escaping out of block elements and splitting block elements
+      doubleLineBreakEscapesBlock: true,
+      // Array (or single string) of stylesheet urls to be loaded in the editor's iframe
+      stylesheets:          [],
+      // Placeholder text to use, defaults to the placeholder attribute on the textarea element
+      placeholderText:      undef,
+      // Whether the rich text editor should be rendered on touch devices (wysihtml5 >= 0.3.0 comes with basic support for iOS 5)
+      supportTouchDevices:  true,
+      // Whether senseless <span> elements (empty or without attributes) should be removed/replaced with their content
+      cleanUp:              true,
+      // Whether to use div instead of secure iframe
+      contentEditableMode: false,
+      classNames: {
+        // Class name which should be set on the contentEditable element in the created sandbox iframe, can be styled via the 'stylesheets' option
+        composer: "wysihtml5-editor",
+        // Class name to add to the body when the wysihtml5 editor is supported
+        body: "wysihtml5-supported",
+        // classname added to editable area element (iframe/div) on creation
+        sandbox: "wysihtml5-sandbox",
+        // class on editable area with placeholder
+        placeholder: "wysihtml5-placeholder",
+        // Classname of container that editor should not touch and pass through
+        uneditableContainer: "wysihtml5-uneditable-container"
+      },
+      // Browsers that support copied source handling will get a marking of the origin of the copied source (for determinig code cleanup rules on paste)
+      // Also copied source is based directly on selection - 
+      // (very useful for webkit based browsers where copy will otherwise contain a lot of code and styles based on whatever and not actually in selection).
+      // If falsy value is passed source override is also disabled
+      copyedFromMarking: '<meta name="copied-from" content="wysihtml5">'
     },
-    // Browsers that support copied source handling will get a marking of the origin of the copied source (for determinig code cleanup rules on paste)
-    // Also copied source is based directly on selection - 
-    // (very useful for webkit based browsers where copy will otherwise contain a lot of code and styles based on whatever and not actually in selection).
-    // If falsy value is passed source override is also disabled
-    copyedFromMarking: '<meta name="copied-from" content="wysihtml5">'
-  };
-
-  wysihtml.Editor = wysihtml.lang.Dispatcher.extend(
-    /** @scope wysihtml.Editor.prototype */ {
+    
     constructor: function(editableElement, config) {
       this.editableElement  = typeof(editableElement) === "string" ? document.getElementById(editableElement) : editableElement;
-      this.config           = wysihtml.lang.object({}).merge(defaultConfig).merge(config).get();
+      this.config           = wysihtml.lang.object({}).merge(this.defaults).merge(config).get();
       this._isCompatible    = wysihtml.browser.supported();
 
       // merge classNames
