2bcc5f9681a4c714eb1562e2af903235a875fbeb
Oliver Pulges <oliver@fraktal.ee>
2016-05-12 16:15:11 +0300

    Remove IE8 polyfills as support is deprecated

--- src/polyfills.js
+++ src/polyfills.js
@@ -2,99 +2,6 @@ wysihtml.polyfills = function(win, doc) {
 
   // TODO: in future try to replace most inline compability checks with polyfills for code readability 
 
-  // IE8 SUPPORT BLOCK
-  // You can compile without all this if IE8 is not needed
-
-  // String trim for ie8
-  if (!String.prototype.trim) {
-    (function() {
-      // Make sure we trim BOM and NBSP
-      var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
-      String.prototype.trim = function() {
-        return this.replace(rtrim, '');
-      };
-    })();
-  }
-
-  // addEventListener, removeEventListener
-  (function() {
-    var s_add = 'addEventListener',
-        s_rem = 'removeEventListener';
-    if( doc[s_add] ) return;
-    win.Element.prototype[ s_add ] = win[ s_add ] = doc[ s_add ] = function( on, fn, self ) {
-      return (self = this).attachEvent( 'on' + on, function(e){
-        var e = e || win.event;
-        e.target = e.target || e.srcElement;
-        e.preventDefault  = e.preventDefault  || function(){e.returnValue = false};
-        e.stopPropagation = e.stopPropagation || function(){e.cancelBubble = true};
-        e.which = e.button ? ( e.button === 2 ? 3 : e.button === 4 ? 2 : e.button ) : e.keyCode;
-        fn.call(self, e);
-      });
-    };
-    win.Element.prototype[ s_rem ] = win[ s_rem ] = doc[ s_rem ] = function( on, fn ) {
-      return this.detachEvent( 'on' + on, fn );
-    };
-  })();
-
-  // element.textContent polyfill.
-  if (Object.defineProperty && Object.getOwnPropertyDescriptor && Object.getOwnPropertyDescriptor(win.Element.prototype, "textContent") && !Object.getOwnPropertyDescriptor(win.Element.prototype, "textContent").get) {
-    (function() {
-      var innerText = Object.getOwnPropertyDescriptor(win.Element.prototype, "innerText");
-      Object.defineProperty(win.Element.prototype, "textContent",
-        {
-          get: function() {
-            return innerText.get.call(this);
-          },
-          set: function(s) {
-            return innerText.set.call(this, s);
-          }
-        }
-      );
-    })();
-  }
-
-  // isArray polyfill for ie8
-  if(!Array.isArray) {
-    Array.isArray = function(arg) {
-      return Object.prototype.toString.call(arg) === '[object Array]';
-    };
-  }
-
-  // Array indexOf for ie8
-  if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(a,f) {
-      for(var c=this.length,r=-1,d=f>>>0; ~(c-d); r=this[--c]===a?c:r);
-      return r;
-    };
-  }
-
-  // Function.prototype.bind()
-  // TODO: clean the code from variable 'that' as it can be confusing
-  if (!Function.prototype.bind) {
-    Function.prototype.bind = function(oThis) {
-      if (typeof this !== 'function') {
-        // closest thing possible to the ECMAScript 5
-        // internal IsCallable function
-        throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-      }
-
-      var aArgs   = Array.prototype.slice.call(arguments, 1),
-          fToBind = this,
-          fNOP    = function() {},
-          fBound  = function() {
-            return fToBind.apply(this instanceof fNOP && oThis
-                   ? this
-                   : oThis,
-                   aArgs.concat(Array.prototype.slice.call(arguments)));
-          };
-
-      fNOP.prototype = this.prototype;
-      fBound.prototype = new fNOP();
-
-      return fBound;
-    };
-  }
-
   // closest and matches polyfill
   // https://github.com/jonathantneal/closest
   (function (ELEMENT) {
@@ -124,245 +31,79 @@ wysihtml.polyfills = function(win, doc) {
 
       return element;
     };
-  }(Element.prototype));
+  }(win.Element.prototype));
 
-  // Element.classList for ie8-9 (toggle all IE)
-  // source http://purl.eligrey.com/github/classList.js/blob/master/classList.js
+  if (!('classList' in doc.documentElement) && win.Object.defineProperty && typeof HTMLElement !== 'undefined') {
+    win.Object.defineProperty(HTMLElement.prototype, 'classList', {
+      get: function() {
+        var self = this;
+        function update(fn) {
+          return function(value) {
+            var classes = self.className.split(/\s+/),
+                index = classes.indexOf(value);
 
-  if ("document" in win) {
-    // Full polyfill for browsers with no classList support
-    if (!("classList" in doc.createElement("_"))) {
-      (function(view) {
-        "use strict";
-        if (!('Element' in view)) return;
-
-        var
-          classListProp = "classList",
-          protoProp = "prototype",
-          elemCtrProto = view.Element[protoProp],
-          objCtr = Object,
-          strTrim = String[protoProp].trim || function() {
-            return this.replace(/^\s+|\s+$/g, "");
-          },
-          arrIndexOf = Array[protoProp].indexOf || function(item) {
-            var
-              i = 0,
-              len = this.length;
-            for (; i < len; i++) {
-              if (i in this && this[i] === item) {
-                return i;
-              }
-            }
-            return -1;
-          }, // Vendors: please allow content code to instantiate DOMExceptions
-          DOMEx = function(type, message) {
-            this.name = type;
-            this.code = DOMException[type];
-            this.message = message;
-          },
-          checkTokenAndGetIndex = function(classList, token) {
-            if (token === "") {
-              throw new DOMEx(
-                "SYNTAX_ERR", "An invalid or illegal string was specified"
-              );
-            }
-            if (/\s/.test(token)) {
-              throw new DOMEx(
-                "INVALID_CHARACTER_ERR", "String contains an invalid character"
-              );
-            }
-            return arrIndexOf.call(classList, token);
-          },
-          ClassList = function(elem) {
-            var
-              trimmedClasses = strTrim.call(elem.getAttribute("class") || ""),
-              classes = trimmedClasses ? trimmedClasses.split(/\s+/) : [],
-              i = 0,
-              len = classes.length;
-            for (; i < len; i++) {
-              this.push(classes[i]);
-            }
-            this._updateClassName = function() {
-              elem.setAttribute("class", this.toString());
-            };
-          },
-          classListProto = ClassList[protoProp] = [],
-          classListGetter = function() {
-            return new ClassList(this);
+            fn(classes, index, value);
+            self.className = classes.join(' ');
           };
-        // Most DOMException implementations don't allow calling DOMException's toString()
-        // on non-DOMExceptions. Error's toString() is sufficient here.
-        DOMEx[protoProp] = Error[protoProp];
-        classListProto.item = function(i) {
-          return this[i] || null;
-        };
-        classListProto.contains = function(token) {
-          token += "";
-          return checkTokenAndGetIndex(this, token) !== -1;
-        };
-        classListProto.add = function() {
-          var
-            tokens = arguments,
-            i = 0,
-            l = tokens.length,
-            token, updated = false;
-          do {
-            token = tokens[i] + "";
-            if (checkTokenAndGetIndex(this, token) === -1) {
-              this.push(token);
-              updated = true;
-            }
-          }
-          while (++i < l);
-
-          if (updated) {
-            this._updateClassName();
-          }
-        };
-        classListProto.remove = function() {
-          var
-            tokens = arguments,
-            i = 0,
-            l = tokens.length,
-            token, updated = false,
-            index;
-          do {
-            token = tokens[i] + "";
-            index = checkTokenAndGetIndex(this, token);
-            while (index !== -1) {
-              this.splice(index, 1);
-              updated = true;
-              index = checkTokenAndGetIndex(this, token);
-            }
-          }
-          while (++i < l);
-
-          if (updated) {
-            this._updateClassName();
-          }
-        };
-        classListProto.toggle = function(token, force) {
-          token += "";
-
-          var
-            result = this.contains(token),
-            method = result ?
-            force !== true && "remove" :
-            force !== false && "add";
-
-          if (method) {
-            this[method](token);
-          }
-
-          if (force === true || force === false) {
-            return force;
-          } else {
-            return !result;
-          }
-        };
-        classListProto.toString = function() {
-          return this.join(" ");
-        };
-
-        if (objCtr.defineProperty) {
-          var classListPropDesc = {
-            get: classListGetter,
-            enumerable: true,
-            configurable: true
-          };
-          try {
-            objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
-          } catch (ex) { // IE 8 doesn't support enumerable:true
-            if (ex.number === -0x7FF5EC54) {
-              classListPropDesc.enumerable = false;
-              objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
-            }
-          }
-        } else if (objCtr[protoProp].__defineGetter__) {
-          elemCtrProto.__defineGetter__(classListProp, classListGetter);
         }
 
-      }(win));
+        var ret = {
+            add: update(function(classes, index, value) {
+              ~index || classes.push(value);
+            }),
 
-    } else if ("DOMTokenList" in win) {
-      // There is full or partial native classList support, so just check if we need
-      // to normalize the add/remove and toggle APIs.
-      // DOMTokenList is expected to exist (removes conflicts with multiple polyfills present on site)
+            remove: update(function(classes, index) {
+              ~index && classes.splice(index, 1);
+            }),
 
-      (function() {
-        "use strict";
+            toggle: update(function(classes, index, value) {
+              ~index ? classes.splice(index, 1) : classes.push(value);
+            }),
 
-        var testElement = doc.createElement("_");
+            contains: function(value) {
+              return !!~self.className.split(/\s+/).indexOf(value);
+            },
 
-        testElement.classList.add("c1", "c2");
-
-        // Polyfill for IE 10/11 and Firefox <26, where classList.add and
-        // classList.remove exist but support only one argument at a time.
-        if (!testElement.classList.contains("c2")) {
-          var createMethod = function(method) {
-            var original = win.DOMTokenList.prototype[method];
-
-            win.DOMTokenList.prototype[method] = function(token) {
-              var i, len = arguments.length;
-
-              for (i = 0; i < len; i++) {
-                token = arguments[i];
-                original.call(this, token);
-              }
-            };
-          };
-          createMethod('add');
-          createMethod('remove');
-        }
-
-        testElement.classList.toggle("c3", false);
-
-        // Polyfill for IE 10 and Firefox <24, where classList.toggle does not
-        // support the second argument.
-        if (testElement.classList.contains("c3")) {
-          var _toggle = win.DOMTokenList.prototype.toggle;
-
-          win.DOMTokenList.prototype.toggle = function(token, force) {
-            if (1 in arguments && !this.contains(token) === !force) {
-              return force;
-            } else {
-              return _toggle.call(this, token);
+            item: function(i) {
+              return self.className.split(/\s+/)[i] || null;
             }
           };
 
-        }
+        win.Object.defineProperty(ret, 'length', {
+          get: function() {
+            return self.className.split(/\s+/).length;
+          }
+        });
 
-        testElement = null;
-      }());
-
-    }
-
+        return ret;
+      }
+    });
   }
 
   // Safary has a bug of not restoring selection after node.normalize correctly.
   // Detects the misbegaviour and patches it
   var normalizeHasCaretError = function() {
-    if ("createRange" in document && "getSelection" in window) {
-      var e = document.createElement('div'),
-          t1 = document.createTextNode('a'),
-          t2 = document.createTextNode('a'),
-          t3 = document.createTextNode('a'),
-          r = document.createRange(),
+    if ("createRange" in doc && "getSelection" in win) {
+      var e = doc.createElement('div'),
+          t1 = doc.createTextNode('a'),
+          t2 = doc.createTextNode('a'),
+          t3 = doc.createTextNode('a'),
+          r = doc.createRange(),
           s, ret;
 
       e.setAttribute('contenteditable', 'true');
       e.appendChild(t1);
       e.appendChild(t2);
       e.appendChild(t3);
-      document.body.appendChild(e);
+      doc.body.appendChild(e);
       r.setStart(t2, 1);
       r.setEnd(t2, 1);
 
-      s = window.getSelection();
+      s = win.getSelection();
       s.removeAllRanges();
       s.addRange(r);
       e.normalize();
-      s = window.getSelection();
+      s = win.getSelection();
 
       ret = (e.childNodes.length !== 1 || s.anchorNode !== e.firstChild || s.anchorOffset !== 2);
       e.parentNode.removeChild(e);
@@ -398,7 +139,7 @@ wysihtml.polyfills = function(win, doc) {
   };
 
   var normalizeFix = function() {
-    var f = Node.prototype.normalize;
+    var f = win.Node.prototype.normalize;
     var nf = function() {
       var texts = getTextNodes(this),
           s = this.ownerDocument.defaultView.getSelection(),
@@ -424,7 +165,7 @@ wysihtml.polyfills = function(win, doc) {
         aelement = undefined;
       }
 
-      if ((anode === fnode && foffset < aoffset) || (anode !== fnode && (anode.compareDocumentPosition(fnode) & Node.DOCUMENT_POSITION_PRECEDING) && !(anode.compareDocumentPosition(fnode) & Node.DOCUMENT_POSITION_CONTAINS))) {
+      if ((anode === fnode && foffset < aoffset) || (anode !== fnode && (anode.compareDocumentPosition(fnode) & win.Node.DOCUMENT_POSITION_PRECEDING) && !(anode.compareDocumentPosition(fnode) & win.Node.DOCUMENT_POSITION_CONTAINS))) {
         fnode = [anode, anode = fnode][0];
         foffset = [aoffset, aoffset = foffset][0];
       }
@@ -463,18 +204,18 @@ wysihtml.polyfills = function(win, doc) {
         s.addRange(r);
       }
     };
-    Node.prototype.normalize = nf;
+    win.Node.prototype.normalize = nf;
   };
   
   var F = function() {
-    window.removeEventListener("load", F);
-    if ("Node" in window && "normalize" in Node.prototype && normalizeHasCaretError()) {
+    win.removeEventListener("load", F);
+    if ("Node" in win && "normalize" in win.Node.prototype && normalizeHasCaretError()) {
       normalizeFix();
     }
   };
   
   if (doc.readyState !== "complete") {
-    window.addEventListener("load", F);
+    win.addEventListener("load", F);
   } else {
     F();
   }
@@ -491,7 +232,7 @@ wysihtml.polyfills = function(win, doc) {
 
   // Polyfills CustomEvent object for IE9 and up
   (function() {
-    if (!customEventSupported && "CustomEvent" in window) {
+    if (!customEventSupported && "CustomEvent" in win) {
       function CustomEvent(event, params) {
         params = params || {bubbles: false, cancelable: false, detail: undefined};
         var evt = doc.createEvent('CustomEvent');
